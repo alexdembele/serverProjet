85988e020d9db978c262c598843de8cb1756508f
alexdembele <chessdembel@gmail.com>
2023-04-02 04:41:39 +0200

    echange donnée sans grille

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <SFML/Network.hpp>
 #include <cmath>
 #include <vector>
+#include<unistd.h>  
 #include <time.h>
 #include<sstream>
 #include<iostream>
@@ -36,22 +37,49 @@ int figures[7][4] =
     2,3,4,5, // O
 };
 
-// regarde si la pièce peut se déplacer
-/*bool check()
-{
-   for (int i=0;i<4;i++)
-      if (a[i].x<0 || a[i].x>=N || a[i].y>=M) return 0;
-      else if (field[a[i].y][a[i].x]) return 0;
 
-   return 1;
-};
 sf::Packet& operator <<(sf::Packet& packet, const Game& game)
 {
+    std::string buffer;
      for (int i=0; i<20; i++)
     {
       for (int j=0;j<10;j++)
       {
-        packet <<  game.grille.grille[i][j];
+        if (game.grille.grille[i][j]==1)
+        {
+          buffer="b";
+          
+        }
+        else if (game.grille.grille[i][j]==2)
+        {
+          buffer="c";
+          
+        }
+        else if (game.grille.grille[i][j]==3)
+        {
+          buffer="d";
+        }
+        else if (game.grille.grille[i][j]==4)
+        {
+          buffer="e";
+        }
+        else if (game.grille.grille[i][j]==5)
+        {
+          buffer="f";
+        }
+        else if (game.grille.grille[i][j]==6)
+        {
+          buffer="g";
+        }
+        else if (game.grille.grille[i][j]==7)
+        {
+          buffer="h";
+        }
+        else
+        {
+          buffer="a";
+        }
+        packet <<    buffer;
       }
      
     }
@@ -60,49 +88,73 @@ sf::Packet& operator <<(sf::Packet& packet, const Game& game)
 
 sf::Packet& operator >>(sf::Packet& packet, Game& game)
 {
+  std::string buffer;
     for (int i=0; i<20; i++)
     {
       for (int j=0;j<10;j++)
       {
-        packet >>  game.grille.grille[i][j];
+        packet >> buffer;
+        std::cout <<buffer<<";";
+        if (buffer==std::string("b"))
+        {
+          game.grille.grille[i][j]==1;
+        }
+        else if (buffer==std::string("c"))
+        {
+          game.grille.grille[i][j]==2;
+        }
+        else if (buffer==std::string("d"))
+        {
+          game.grille.grille[i][j]==3;
+        }
+        else if (buffer==std::string("e"))
+        {
+          game.grille.grille[i][j]==4;
+        }
+        else if (buffer==std::string("f"))
+        {
+          game.grille.grille[i][j]==5;
+        }
+        else if (buffer==std::string("g"))
+        {
+          game.grille.grille[i][j]==6;
+        }
+        else if (buffer==std::string("h"))
+        {
+          game.grille.grille[i][j]==7;
+        }
+        else
+        {
+          game.grille.grille[i][j]==0;
+        }
       }
      
     }
     
     return packet ;
-}*/
-
+}
 int main()
 {
-    /*srand(time(0));     
-    //chargement graphisme
-    //RenderWindow window(VideoMode(1800, 1000), "The Game!");
-
-    Texture t1,t2,t3;
-    if(!t1.loadFromFile("../Projet/images/tiles.png"))
-      printf("Erreur chargement\n");
-    if(!t2.loadFromFile("../Projet/images/background.png"))
-      printf("Erreur chargement\n");
-    if(!t3.loadFromFile("../Projet/images/frame.png"))
-      printf("Erreur chargement\n");
+  
+    std::string compteur;
+    std::string compteure;
     
-    Sprite s(t1), background(t2), frame(t3),fram(t3);
-    fram.move(Vector2f(450.f,0));
-    scaleToMinSize(background,1900,1100);*/
-
+    Grille grille1;
+    Piece piece1;
+    Game game1(grille1,piece1,false);
     //definition des parametres de jeu
     
     std::cout << sf::IpAddress::getPublicAddress( ) << "\n";
     sf::Packet packet;
     std::string s ;
     sf::TcpListener listener;
-    //listener.setBlocking(false);
+    listener.setBlocking(false);
     // lie l'écouteur à un port
     if (listener.listen(52000) != sf::Socket::Done)
     {
         printf("Erreur port\n");
     }
-
+    std::cout <<"Yousk\n";
     // accepte une nouvelle connexion
     sf::TcpSocket client;
     //client.setBlocking(false);
@@ -111,81 +163,58 @@ int main()
     {
         printf("Wait client\n");
     }
+
+
+
+    //sleep(10);
+    
     client.receive(packet);
     if (packet >> s)
     {
-      std::cout <<"data:"<< s << "\n" <<std::endl;
+      std::cout <<"data:"<< s << "\n" ;
+      std::cout <<"you";
     }
+    
     else
     {
         std::cout <<"erreur"<< "\n" <<std::endl;
     }
-
+    
+    //client.setBlocking(false);
+    while (true)
+    {
       
-    
-    /*while (true)
-    {
-
-      std::cout <<"data:"<< data << "\n" <<std::endl;
-    }*/
-
-
-    /*
-    Clock clock;
-
-    //TEST
-    //definition de la grille
-    
-    Grille Tertest;
-    
-    //definition de la piece
-   
-    Piece piecTest;
-
-    //definition de la partie
-    Game myGame(Tertest,piecTest);
-    //definition temps
-    float temps=0;
-    
-    //std::cout << sf::IpAddress::getPublicAddress( ) << "\n";
-    while (window.isOpen())
-    {
-        float time = clock.getElapsedTime().asSeconds();
-        clock.restart();
-        
-        temps+=time;
-
-        Event e;
-        while (window.pollEvent(e))
-        {
-            if (e.type == Event::Closed)
-                window.close();
-
-            
-              myGame.commande(e);
-        }
+      packet.clear();
+      //reception grille;
       client.receive(packet);
-      packet >> myGame;
-    
-    
-   
-    
-    
-    
-        
-    if (myGame.updateGame(temps))
-    {
-      temps=0;
-    }
-    
-    
+      
+      
+      packet >> game1;
+      packet.clear();
+      std::cout <<"Youskboucle\n";
+      
      
 
-   
-    //window.draw(myGame);
+      
+     for (int i=0; i<20; i++)
+      {
+      for (int j=0;j<10;j++)
+      {
+        std::cout <<  game1.grille.grille[i][j]<<";";
+      }
+     
+    }
+    packet<<game1;
+    client.send(packet);
+    packet.clear();
+    sleep(1);
+      
+      
     
-    window.display();
-    }*/
+    }
+
+
+    
 
     return 0;
 }
